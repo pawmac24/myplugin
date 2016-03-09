a590b295d7f521e39460ff167e0cb3594563e30f
Pawel M <pawmac24@gmail.com>
2016-03-09 17:24:10 +0100

    Adding common methods  in webwork

--- src/main/java/com/atlassian/plugins/tutorial/myplugin/jira/webwork/MyWebworkAction.java
+++ src/main/java/com/atlassian/plugins/tutorial/myplugin/jira/webwork/MyWebworkAction.java
@@ -1,16 +1,38 @@
 package com.atlassian.plugins.tutorial.myplugin.jira.webwork;
 
+import com.atlassian.jira.web.action.JiraWebActionSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.atlassian.jira.web.action.JiraWebActionSupport;
 
+//@Component
 public class MyWebworkAction extends JiraWebActionSupport
 {
     private static final Logger log = LoggerFactory.getLogger(MyWebworkAction.class);
 
+//    @ComponentImport
+//    private ApplicationProperties applicationProperties;
+
+//    @Autowired
+//    public MyWebworkAction(ApplicationProperties applicationProperties) {
+//        this.applicationProperties = applicationProperties;
+//    }
+
     @Override
     public String execute() throws Exception {
-
+        log.debug("=== Entering execute ===");
         return super.execute(); //returns SUCCESS
+    }
+    protected void doValidation() {
+        log.debug("=== Entering doValidation ===");
+    }
+
+    protected String doExecute() throws Exception {
+        log.debug("=== Entering doExecute ===");
+        return SUCCESS;
+    }
+
+    public String doDefault() throws Exception {
+        log.debug("=== Entering doDefault ===");
+        return super.doDefault();
     }
 }
