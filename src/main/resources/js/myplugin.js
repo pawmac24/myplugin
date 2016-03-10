81062781ada5f3fad3dc35bce25db11f0512dfe3
Pawel M <pawmac24@gmail.com>
2016-03-10 17:12:10 +0100

    changes in commandtwo and javascript

--- src/main/resources/js/myplugin.js
+++ src/main/resources/js/myplugin.js
@@ -4,26 +4,29 @@
 //});
 
 
-(function ($) {
+//(function ($) {
+//
+//    $(document).ready(function() {
+//        alert("This is myplugin javascript, path: " + AJS.contextPath());
+//
+//        var dialog = new AJS.Dialog({
+//            width: 800,
+//            height: 500,
+//            id: "example-dialog",
+//            closeOnOutsideClick: true
+//        });
+//
+//        dialog.addHeader("Dialog");
+//
+//        dialog.addButton("Cancel", function (dialog) {
+//            dialog.hide();
+//        });
+//
+//        dialog.show();
+//    });
+//
+//})(AJS.$ || jQuery);
 
-    $(document).ready(function() {
-        alert("This is myplugin javascript, path: " + AJS.contextPath());
-
-        var dialog = new AJS.Dialog({
-            width: 800,
-            height: 500,
-            id: "example-dialog",
-            closeOnOutsideClick: true
-        });
-
-        dialog.addHeader("Dialog");
-
-        dialog.addButton("Cancel", function (dialog) {
-            dialog.hide();
-        });
-
-        dialog.show();
-    });
-
-})(AJS.$ || jQuery);
-
+function mytestfun(){
+    alert("testowy javascript");
+};
